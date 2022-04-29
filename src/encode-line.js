fb78e051d3df0356867d465ce8ab038f759fb683
bggth <macair@MacBook-MacAir.local>
2022-04-29 21:17:23 +0300

    fix: add encode-line task

--- src/encode-line.js
+++ src/encode-line.js
@@ -10,9 +10,38 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+
+function onChar(char) {
+
+}
+
+function encodeLine(str) {
+
+  let prevChar = str[0];
+  let charCounter = 1;
+  let result = '';
+
+  for (let i = 1; i < str.length; i++) {
+    if (str[i] == prevChar) {
+      charCounter++
+    } else {
+      if (charCounter!=1) 
+        result = result + charCounter.toString()+prevChar;
+      else
+      result = result + prevChar;
+
+      charCounter = 1;
+      prevChar = str[i];
+    }
+  }
+
+  if (charCounter!=1) 
+    result = result + charCounter.toString()+prevChar;
+  else
+    result = result + prevChar;
+
+  return result;
+
 }
 
 module.exports = {
