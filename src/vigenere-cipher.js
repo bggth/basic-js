b052bdf7691cbde82bb4619c714b2a7e6f94be08
bggth <macair@MacBook-MacAir.local>
2022-04-30 16:56:43 +0300

    fix: add vigenere-chiper solution

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,13 +20,59 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(direct) {
+    this.direct = direct;
+    this.flag = 1;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  encrypt(message, key) {
+    let result = '';
+    if ((message == null) || (key == null))
+      throw new Error('Incorrect arguments!');
+
+    message = message.toUpperCase();
+    //message = this.direct?message:this.reverse(message)
+    key = key.toUpperCase();
+
+    let keyIndex = 0;
+    for (let i = 0; i < message.length; i++) {
+      if ((message[i].charCodeAt(0) >= 'A'.charCodeAt(0)) && (message[i].charCodeAt(0) <= 'Z'.charCodeAt(0))) {
+        //console.debug(message[i], key[keyIndex%key.length],   this.encodeChar(message[i], key[keyIndex%key.length], this.flag));
+        result += this.encodeChar(message[i], key[keyIndex%key.length], this.flag);
+        keyIndex++;
+      } else {
+        result += message[i];
+      }
+    }
+    //console.debug(result);
+    return result;//this.direct?result:this.reverse(result);
+  }
+  decrypt(message, key) {
+    if ((message == null) || (key == null))
+      throw new Error('Incorrect arguments!');
+    this.flag = -this.flag;
+    //message = this.direct?message:this.reverse(message)
+    let result = this.encrypt(message, key);
+    this.flag = -this.flag;
+    return result;
+  }
+
+  encodeChar(char, key, flag) {
+    let result = '';
+    let charCode = char.charCodeAt(0);
+    let delta = key.charCodeAt(0)-'A'.charCodeAt(0);
+    //console.debug(charCode, keyCode);
+    let resultCode = charCode + this.flag*delta;
+    if (resultCode > 'Z'.charCodeAt(0))
+      resultCode = resultCode - 26;      
+    if (resultCode < 'A'.charCodeAt(0))
+      resultCode = resultCode + 26;
+
+    result = String.fromCharCode(resultCode);
+    return result;
+  }
+
+  reverse(str) {
+    return str.split('').reverse().join('');
   }
 }
 
