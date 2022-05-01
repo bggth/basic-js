bf6c246b5d33db87ac976f2c0d5577570273e929
bggth <macair@MacBook-MacAir.local>
2022-05-01 07:22:43 +0300

    fix: transform-array solution

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,9 +13,71 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+
+class MyArray {
+  constructor() {
+    this.data = [];
+    this.doubleNextFlag = false;
+    this.discardNextFlag = false;
+  }
+
+  push(e) {
+    if (this.checkCommand(e) == false) {
+
+      if (this.doubleNextFlag) {
+        this.data.push(e)
+        this.doubleNextFlag = false;
+      }
+
+      if (this.discardNextFlag) {
+        this.data.pop();
+        this.discardNextFlag = false;
+      }
+
+      this.data.push(e);
+    }
+
+  }
+
+  checkCommand(e) {
+    let result = true;;
+    const commands = ['--discard-next', '--discard-prev', '--double-next', '--double-prev'];
+    let command = commands.indexOf(e);
+    switch (command) {
+      case -1:
+        result = false;
+        break;
+      case 0:
+        this.discardNextFlag = true;
+        break;
+      case 1:
+        this.data.pop();
+        break;
+      case 2:
+        this.doubleNextFlag = true;
+        break;
+      case 3:
+        if (this.data.length > 0)
+          this.data.push(this.data[this.data.length-1]);
+        break;
+    }
+    return result;
+  }
+}
+
+function transform(arr) {
+
+  //console.debug(arr);
+
+  if (arr instanceof Array == false)
+    throw new Error(`'arr' parameter must be an instance of the Array!`);
+
+  let result = new MyArray();
+
+  for (let i = 0; i < arr.length; i++) {
+    result.push(arr[i]);
+  }
+  return result.data;
 }
 
 module.exports = {
